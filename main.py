51238d0e6af832ef0b2c0807ad56dc3d9017aecf
dgenutis <dainiusgenutis@yahoo.com>
2023-03-30 20:40:20 +0300

    +parodyti zurnala

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from modules import Irasas
-zurnalas = []
+import pickle
+
+def nuskaityti_is_failo():
+    try:
+        with open("zurnalas.pkl", "rb") as file:
+            zurnalas = pickle.load(file)
+    except:
+        zurnalas = []
+    return zurnalas
+zurnalas = nuskaityti_is_failo()
+
+def irasyti_i_faila():
+    with open("zurnalas.pkl", "wb") as file:
+        pickle.dump(zurnalas, file)
 
 
 while True:
@@ -9,10 +22,12 @@ while True:
             suma = int(input("Įveskite sumą: "))
             irasas = Irasas(suma, "Pajamos")
             zurnalas.append(irasas)
+            irasyti_i_faila()
         case 2:
             suma = int(input("Įveskite sumą: "))
             irasas = Irasas(suma, "Išlaidos")
             zurnalas.append(irasas)
+            irasyti_i_faila()
         case 3:
             print(zurnalas)
         case 4:
