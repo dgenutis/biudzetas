0ef394001213664f83e9b23581706fde88f5eaaf
dgenutis <dainiusgenutis@yahoo.com>
2023-03-30 20:29:57 +0300

    +parodyti zurnala

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ zurnalas = []
 
 
 while True:
-    pasirinkimas = int(input("1 - įvesti pajamas\n2 - įvesti išlaidas\n3 - atvaizduoti žurnalą\n4 - išeiti\n"))
+    pasirinkimas = int(input("1 - įvesti pajamas\n2 - įvesti išlaidas\n3 - atvaizduoti žurnalą\n4 - balansas\n5 - išeiti\n"))
     match pasirinkimas:
         case 1:
             suma = int(input("Įveskite sumą: "))
@@ -16,6 +16,14 @@ while True:
         case 3:
             print(zurnalas)
         case 4:
+            balansas = 0
+            for irasas in zurnalas:
+                if irasas.tipas == "Pajamos":
+                    balansas += irasas.suma
+                if irasas.tipas == "Išlaidos":
+                    balansas -= irasas.suma
+            print("Balansas:", balansas)
+        case 5:
             print("Viso gero")
             break
 
